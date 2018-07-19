9950eafab473a2351b65927c638305b1fa24cbf9
wangxu <1115310504@qq.com>
2018-07-19 18:15:18 +0800

    new

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <!--<img src="./assets/logo.png">-->
+        <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
+        <BackColor />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
+    import BackColor from './components/backColor.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'app',
+        components: {
+            HelloWorld,
+            BackColor
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        width: 100%;
+        height: 100%;
+    }
 </style>
---
+++ src/components/backColor.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="backColorDiv"></div>
+</template>
+
+<script>
+    export default {
+        name: 'BackColor',
+        created: function () {
+            if ('serviceWorker' in navigator) {
+                navigator.serviceWorker && navigator.serviceWorker.register('../service-worker.js').then(() => {
+                    //
+                });
+            }
+            // 「弹出添加到主屏对话框」事件
+            window.addEventListener('beforeinstallprompt', event => {
+                // 这个 `event.userChoice` 是一个 Promise ，在用户选择后 resolve
+                event.userChoice.then(result => {
+                    console.log(result.outcome)
+                    // 'accepted': 添加到主屏
+                    // 'dismissed': 用户不想理你并向你扔了个取消
+                })
+            })
+        }
+    }
+</script>
+
+<style scoped>
+    .backColorDiv {
+        width: 100%;
+        height: 100%;
+        background: url("../../public/color.jpg") no-repeat 50% 50%;
+    }
+</style>
